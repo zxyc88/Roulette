ef930934d5b7f18a1612acab11a455d6a3bb7999
mo <nasarmo72@gmail.com>
2019-11-25 14:16:28 -0500

    working DB

--- Schema/create_db_postgres.sql
+++ Schema/create_db_postgres.sql
@@ -1,14 +1,14 @@
 CREATE TABLE public."dealers"
 (
     id serial primary key,
-	name varchar(255) NOT NULL
+	  name varchar(255) NOT NULL
 );
 
 
 CREATE TABLE public."view_data"
 (
     id serial primary key,
-	dealer_id integer NOT NULL REFERENCES dealers (id),
+	  dealer_id integer NOT NULL REFERENCES dealers (id),
     start_at numeric(5),
     finish_at numeric(5)
 );
